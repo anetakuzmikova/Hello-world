de18beb07005dd5e6c82b31e6b850e4fadbede6a
anetakuzmikova <91833867+anetakuzmikova@users.noreply.github.com>
2021-10-16 23:56:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,24 +14,20 @@ string jmeno, prijmeni;
 vek=0;
 vyska=0;
 vzdelani=0;
+jmeno="mojekrestnijmeno";
+prijmeni="mojeprijmeni";
 
 
 vek=nacti_vek (vek, 0,130);
 vyska=nacti_vysku (vyska, 150,250);
 vzdelani=nacti_vzdelani (vzdelani);
+nacti_jmeno(jmeno, prijmeni);
+uloz_do_souboru(jmeno,prijmeni,vek,vyska,vzdelani);
 
 
-cout<<"\nkontrolni vypis: vek, vyska, vzdelani:  "<<vek<<",  "<<vyska<<",  "<<vzdelani<<",  "<<jmeno<<"\n";
-
+cout<<"\nkontrolni vypis po fcich: vek, vyska, vzdelani:  "<<vek<<",  "<<vyska<<",  "<<vzdelani<<",  "<<jmeno<<" "<<prijmeni<<"\n";
+}
 
 //string jmeno,prijmeni;
 //cout<<"zadejte jmeno ve tvaru 'jmeno prijmeni'\n";
 //cin>>jmeno>>prijmeni;
-
-}
-
-
-
-
-
-
