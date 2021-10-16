d3953b1252ca0009d272e1ed9b3ab113e23b2f5d
anetakuzmikova <91833867+anetakuzmikova@users.noreply.github.com>
2021-10-16 14:54:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
-#include <stdio.h>
-#include <math.h>
+#include <fstream>
+#include "hlavickovy_soubor.h"
+#include <string>
 
 
+using namespace std;
 int main()
 {
-  std::cout << "Hello World!\n";
-  printf("Aneta Kuzmikova");
-  return 0;
+int vyska,vzdelani,vek;
+string jmeno, prijmeni;
+
+
+vek=0;
+vyska=0;
+vzdelani=0;
+
+
+vek=nacti_vek (vek, 0,130);
+vyska=nacti_vysku (vyska, 150,250);
+vzdelani=nacti_vzdelani (vzdelani);
+
+
+cout<<"\nkontrolni vypis: vek, vyska, vzdelani:  "<<vek<<",  "<<vyska<<",  "<<vzdelani<<",  "<<jmeno<<"\n";
+
+
+//string jmeno,prijmeni;
+//cout<<"zadejte jmeno ve tvaru 'jmeno prijmeni'\n";
+//cin>>jmeno>>prijmeni;
+
 }
+
+
+
+
+
+
