12d7d90211694033792357cdb4c63ba98ccc98b6
anetakuzmikova <91833867+anetakuzmikova@users.noreply.github.com>
2021-10-17 10:25:46 +0200

    Update hlavickovy_subor.cpp

--- hlavickovy_subor.cpp
+++ hlavickovy_subor.cpp
@@ -1,54 +1,46 @@
-#include"hlavickovy_soubor.h"
+#include"clovek.h"
 #include<iostream>
 #include<fstream>
 #include<string>
 using namespace std;
 
-int nacti_vek(int& vek,int vek_min, int vek_max)
+void nacti_udaje(std::string& jmeno, std::string& prijmeni, int& vek, int& vyska, int& vzdelani)
 {
-    do
-    {
+    int vek_min, vek_max, vyska_min, vyska_max;
+    vek_min=0;
+    vek_max=130;
+    vyska_min=150;
+    vyska_max=250;
+
+    cout<<"zadejte cele jmeno ve tvaru 'krestni jmeno mezera prijmeni'";
+    cin>>jmeno>>prijmeni;
+
+    do {
             cin.clear();
             cin.ignore(1000,'\n');
             cout<<"zadejte vek";
             cin>>vek;
-    }
+       }
     while (cin.fail()  || vek < vek_min || vek > vek_max);
-    return vek;
-}
 
-
-int nacti_vysku(int& vyska, int nejmensi_vyska, int nejvetsi_vyska)
-{
-        do{
+    do {
             cin.clear();
             cin.ignore(1000,'\n');
             cout<<"zadejte vysku v centimetrech";
             cin>>vyska;
-          }
-          while (cin.fail()  || vyska < nejmensi_vyska || vyska > nejvetsi_vyska);
-          return vyska;
-   }
+       }
+    while (cin.fail()  || vyska < vyska_min || vyska > vyska_max);
 
-
-int nacti_vzdelani(int& vzdelani)
-{
-        do{
+    do {
             cin.clear();
             cin.ignore(1000,'\n');
             cout<<"zadejte dosazene vzdelani: 1 pro zakladni, 2 pro stredoskolske, 3 pro vysokoskolske";
             cin>>vzdelani;
-          }
+       }
           while (cin.fail()  || vzdelani<1 || vzdelani > 3);
-          return vzdelani;
-}
-
-void nacti_jmeno(string& jmeno, string& prijmeni)
-{
-    cout<<"zadejte cele jmeno ve tvaru 'krestni jmeno mezera prijmeni'";
-    cin>>jmeno>>prijmeni;
 
 }
+
 
 void uloz_do_souboru (string vypis_jmeno, string vypis_prijmeni, int vypis_vek, int vypis_vyska, int vypis_vzdelani)
 //neni nutne pouzivat reference,protoze fce nemeni hodnoty promennych v mainu(?)
@@ -88,4 +80,5 @@ void uloz_do_souboru (string vypis_jmeno, string vypis_prijmeni, int vypis_vek, 
           //while (cin.fail()  || vek < nejmensi_vek || vek > nejvetsi_vek);
           //return vek;
 //}
+
 
